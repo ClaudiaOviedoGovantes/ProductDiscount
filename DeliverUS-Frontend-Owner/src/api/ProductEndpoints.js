340cff06b7c54fef5be968293e3289c1b081f054
ClaudiaOviedoGovantes <claudiaogvizcaino@gmail.com>
2024-05-23 22:25:20 +0200

    Solved?

---
+++ DeliverUS-Frontend-Owner/src/api/ProductEndpoints.js
@@ -0,0 +1,30 @@
+import { get, post, put, destroy, patch } from './helpers/ApiRequestsHelper'
+
+function getDetail (id) {
+  return get(`products/${id}`)
+}
+
+function getProductCategories () {
+  return get('productCategories')
+}
+
+function create (data) {
+  return post('/products/', data)
+}
+
+function update (id, data) {
+  return put(`products/${id}`, data)
+}
+
+function remove (id) {
+  return destroy(`products/${id}`)
+}
+// BEGIN SOLUTION
+// Enpoint for toggling promote property
+function promote (id) {
+  return patch(`products/${id}/togglepromoted`)
+}
+
+// Added promote endpoint handler
+export { getDetail, getProductCategories, create, update, remove, promote }
+// END SOLUTION
